75adcddd58b5390fbef2241bbedbbd8a7890b70f
Gunter <82664008+ramses203@users.noreply.github.com>
2021-07-28 18:27:07 +0900

    Add files via upload

---
+++ Oracle_Database/select.sql
@@ -0,0 +1,5 @@
+select * from countries;
+select * from departments;
+select department_name from departments;
+select department_id, department_name from departments;
+select employee_id, first_name, last_name, email, phone_number, hire_date from employees;
