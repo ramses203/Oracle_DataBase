7cbbd9a118a252481c0a49a41a164cb5ce6fa08f
Gunter <82664008+ramses203@users.noreply.github.com>
2021-07-30 02:00:36 +0900

    Delete select.sql

--- Oracle_Database/select.sql
+++
@@ -1,5 +0,0 @@
-select * from countries;
-select * from departments;
-select department_name from departments;
-select department_id, department_name from departments;
-select employee_id, first_name, last_name, email, phone_number, hire_date from employees;
