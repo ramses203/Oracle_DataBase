9f264b8f07c1f55d4d002c36c7177476fc35d77a
Gunter <82664008+ramses203@users.noreply.github.com>
2021-08-07 23:36:41 +0900

    Add files via upload

--- Oracle_Database/chapter14.sql
+++ Oracle_Database/chapter14.sql
@@ -165,6 +165,77 @@ dbms_output.put_line(v_eno||'          '||v_ename);
 end;
 /
 
+set serveroutput on
+declare
+v_employee employee%rowtype;
+temp number(4);
+annsal number(7,2);
+begin
+select * into v_employee
+from employee
+where ename = 'SCOTT';
+if v_employee.commission is null then
+v_employee.commission :=0;
+end if;
+
+annsal := v_employee.salary * 12 + v_employee.commission;
+
+dbms_output.put_line('사원번호     사원이름     연봉');
+dbms_output.put_line('---------------------------');
+dbms_output.put_line(v_employee.eno||'        '||v_employee.ename||'      '||annsal);
+end;
+/
+
+
+
+set serveroutput on
+declare
+v_eno employee.eno%type;
+v_ename employee.ename%type;
+v_dno employee.dno%type;
+v_dname varchar2(20) := null;
+begin
+select eno, ename, dno into v_eno, v_ename, v_dno
+from employee
+where ename = 'SCOTT';  
+if(v_dno = 10)then
+v_dname := 'ACCOUNTING';
+elsif(v_dno = 20)then
+v_dname := 'RESEARCH';
+elsif(v_dno = 30)then
+v_dname := 'SALES';
+elsif(v_dno = 40)then
+v_dname := 'OPERATIONS';
+end if;
+dbms_output.put_line('사원번호    이름    부서명');
+dbms_output.put_line('=======================');
+dbms_output.put_line(v_eno||'    '||v_ename||'    '||v_dname);
+end;
+/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
